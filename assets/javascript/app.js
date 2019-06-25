98c4efda109e6bd423c69f9e5e2ada1a77175bdc
Patricia <correiaph13@gmail.com>
2019-06-25 17:29:48 -0500

    JS DOM mods

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,50 @@
 // On click, hide home page
 
-$("#aboutLink").click(function(){
-    $("#jumboSection").hide(1000);
-    $("#aboutSection").show(1000);
+$("#hibiLogo").on("click", function(){
+  $("#jumboSection").show(1000);
+    $("#aboutSection").hide(1000);
+    $("#portfolioSection").hide(1000);
+    $("#experienceSection").hide(1000);
+    $("#contactSection").hide(1000);
   });
+
+  
+  $("#aboutLink").on("click", function(){
+    $("#aboutSection").show(1000);
+    $("#jumboSection").hide(1000);
+    $("#portfolioSection").hide(1000);
+    $("#experienceSection").hide(1000);
+    $("#contactSection").hide(1000);
+  });
+
+
+  $("#portfolioLink").on("click", function(){
+    $("#portfolioSection").show(1000);
+    $("#aboutSection").hide(1000);
+    $("#jumboSection").hide(1000);
+    $("#experienceSection").hide(1000);
+    $("#contactSection").hide(1000);
+  });
+  
+  
+    $("#experienceLink").on("click", function(){
+      $("#experienceSection").show(1000);
+      $("#portfolioSection").hide(1000);
+      $("#aboutSection").hide(1000);
+      $("#jumboSection").hide(1000);
+      $("#contactSection").hide(1000);
+    });
+  
+
+  
+    $("#contactLink").on("click", function(){
+      $("#contactSection").show(1000);
+      $("#experienceSection").hide(1000);
+      $("#portfolioSection").hide(1000);
+      $("#aboutSection").hide(1000);
+      $("#jumboSection").hide(1000);
+    });
+  
+
+
+
