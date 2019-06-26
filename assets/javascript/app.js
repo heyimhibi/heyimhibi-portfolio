6f87fcbb664bd9b686f89130491512c126948d3b
Patricia <correiaph13@gmail.com>
2019-06-25 20:11:02 -0500

    added some content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // On click, hide home page
 $( document ).ready(function() {
-  $("#jumboSection").show(1000);
+  $("#jumboSection").show(500);
     $("#aboutSection").hide();
     $("#portfolioSection").hide();
     $("#experienceSection").hide();
@@ -8,48 +8,44 @@ $( document ).ready(function() {
 });
 
 $("#hibiLogo").on("click", function(){
-  $("#jumboSection").show(1000);
-    $("#aboutSection").hide(1000);
-    $("#portfolioSection").hide(1000);
-    $("#experienceSection").hide(1000);
-    $("#contactSection").hide(1000);
+  $("#jumboSection").show(500);
+    $("#aboutSection").hide(500);
+    $("#portfolioSection").hide(500);
+    $("#experienceSection").hide(500);
+    $("#contactSection").hide(500);
   });
 
-  
   $("#aboutLink").on("click", function(){
-    $("#aboutSection").show(1000);
-    $("#jumboSection").hide(1000);
-    $("#portfolioSection").hide(1000);
-    $("#experienceSection").hide(1000);
-    $("#contactSection").hide(1000);
+    $("#aboutSection").show(500);
+    $("#jumboSection").hide(500);
+    $("#portfolioSection").hide(500);
+    $("#experienceSection").hide(500);
+    $("#contactSection").hide(500);
   });
-
 
   $("#portfolioLink").on("click", function(){
-    $("#portfolioSection").show(1000);
-    $("#aboutSection").hide(1000);
-    $("#jumboSection").hide(1000);
-    $("#experienceSection").hide(1000);
-    $("#contactSection").hide(1000);
+    $("#portfolioSection").show(500);
+    $("#aboutSection").hide(500);
+    $("#jumboSection").hide(500);
+    $("#experienceSection").hide(500);
+    $("#contactSection").hide(500);
   });
   
-  
     $("#experienceLink").on("click", function(){
-      $("#experienceSection").show(1000);
-      $("#portfolioSection").hide(1000);
-      $("#aboutSection").hide(1000);
-      $("#jumboSection").hide(1000);
-      $("#contactSection").hide(1000);
+      $("#experienceSection").show(500);
+      $("#portfolioSection").hide(500);
+      $("#aboutSection").hide(500);
+      $("#jumboSection").hide(500);
+      $("#contactSection").hide(500);
     });
   
 
-  
     $("#contactLink").on("click", function(){
-      $("#contactSection").show(1000);
-      $("#experienceSection").hide(1000);
-      $("#portfolioSection").hide(1000);
-      $("#aboutSection").hide(1000);
-      $("#jumboSection").hide(1000);
+      $("#contactSection").show(500);
+      $("#experienceSection").hide(500);
+      $("#portfolioSection").hide(500);
+      $("#aboutSection").hide(500);
+      $("#jumboSection").hide(500);
     });
   
 
